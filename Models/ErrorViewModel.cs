ce525431f394fe6b24331b3c496633b42cb7f646
jos <josmerjauriacaceria@gmail.com>
2025-03-28 21:52:59 -0500

    versión mvc

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace prac1_lab_cjjosmer.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
